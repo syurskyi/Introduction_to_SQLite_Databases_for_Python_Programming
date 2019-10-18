6fe82349c608774e9300d9e46e43e4e4fa350255
syurskyi <sergejyurskyj@yahoo.com>
2019-10-17 17:27:51 -0700

    Update 001.Connecting SQLite to the Database.py

--- 001.Connecting SQLite to the Database.py
+++ 001.Connecting SQLite to the Database.py
@@ -5,3 +5,12 @@ import sqlite3
 # Creates or opens a file called mydb with a SQLite3 DB
 db = sqlite3.connect('mydb.db')
 db.close()
+
+
+
+
+# https://www.pythoncentral.io/introduction-to-sqlite-in-python/
+# https://www.tutorialspoint.com/sqlite/sqlite_python.htm
+# http://zetcode.com/db/sqlitepythontutorial/
+# https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
+# https://www.sqlitetutorial.net/sqlite-python/
